8f683d4f4226057a2a5cc0a819803bd1e0c83ca8
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-17 07:35:43 -0300

    teste

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,10 @@ const main = document.querySelector('main');
 const img = document.createElement('img');
 main.append(img);
 img.setAttribute('src', link[0]);
+
+let pokeCIRCULO = document.getElementById('teste')
+const poke = document.getElementsByClassName('container_itens_poke')
+poke.addEventListener('mouseover', () => {
+    pokeCIRCULO.setAttribute('id','link')
+
+})
