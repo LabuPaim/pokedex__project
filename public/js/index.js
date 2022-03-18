e0992fa0546928a272474fd53bcda418bec0795d
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-17 22:05:27 -0300

    att dupla

--- public/js/index.js
+++ public/js/index.js
@@ -7,9 +7,17 @@ const img = document.createElement('img');
 main.append(img);
 img.setAttribute('src', link[0]);
 
-let pokeCIRCULO = document.getElementById('teste')
-const poke = document.getElementsByClassName('container_itens_poke')
-poke.addEventListener('mouseover', () => {
-    pokeCIRCULO.setAttribute('id','link')
 
+const pokeLINE1 = document.getElementById('poke-line1')
+const pokeLINE2 = document.getElementById('poke-line2')
+const poke = document.getElementById('container_navbar_itens_pokedex')
+
+poke.addEventListener('mouseover', () => {    
+    pokeLINE1.style.backgroundColor ='#fff';
+    pokeLINE2.style.backgroundColor ='#fff';    
+})
+
+poke.addEventListener('mouseleave', () => {    
+    pokeLINE1.style.backgroundColor ='rgb(85, 84, 84)';
+    pokeLINE2.style.backgroundColor ='rgb(85, 84, 84)';    
 })
