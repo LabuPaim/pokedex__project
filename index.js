03ea1465a8bf2b6628c7469c4c7c607a68b9175f
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-22 18:20:19 -0300

    v 3.1

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
 
 const pokedex = [
   {
-    id: "001",
-    nome: "Bulbasaur",
+    id: '001',
+    nome: 'Bulbasaur',
     tipo: 'Planta',
     categoria: 'Semente',
     habilidade: 'Superar',
     altura: 50,
-    peso: "15",
+    peso: '15',
     imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
     descricao: 'Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.',
   },
   {
-    id: "004",
-    nome: "Charmander",
+    id: '004',
+    nome: 'Charmander',
     tipo: 'Fogo',
     categoria: 'Lagarto',
     habilidade: 'Chama',
     altura: 60,
-    peso: "8,5",
+    peso: '8,5',
     imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
     descricao: 'Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta de sua cauda.',
   },
-]
+];
 
-app.get("/", (req, res) => {
-  res.render("index", {pokedex});
+app.get('/', (req, res) => {
+  res.render('index', { pokedex });
 });
 
-app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`)
-);
+app.post('/add', (req, res) => {
+  const pokemon = req.body;
+  pokedex.push(pokemon);
+  res.redirect('/');
+});
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,24 @@
 const addCLICK = document.getElementById('card_add');
 const formCLICK = document.getElementById('card_form');
 
+const card = document.getElementById('card');
+const lista = document.getElementById('lista');
+
 addCLICK.addEventListener('click', () => {
   formCLICK.style.display = 'flex';
   addCLICK.style.display = 'none';
 });
 
+for (pokemon of {pokedex}) {
+  let container_card = document.getElementById('container_card' + pokemon.id);
+  
+  container_card.addEventListener('mouseover', () => {
+    lista.style.display = 'flex';
+    card.style.display = 'none';
+  });
+
+  container_card.addEventListener('mouseout', () => {
+    lista.style.display = 'none';
+    card.style.display = 'flex';
+  });
+}
