afcd8e26ade0555d7d9631d23ea8b259c79bed95
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-22 20:08:09 -0300

    att 3.3

--- public/js/script.js
+++ public/js/script.js
@@ -9,16 +9,16 @@ addCLICK.addEventListener('click', () => {
   addCLICK.style.display = 'none';
 });
 
-for (pokemon of {pokedex}) {
-  let container_card = document.getElementById('container_card' + pokemon.id);
-  
-  container_card.addEventListener('mouseover', () => {
-    lista.style.display = 'flex';
-    card.style.display = 'none';
-  });
 
-  container_card.addEventListener('mouseout', () => {
-    lista.style.display = 'none';
-    card.style.display = 'flex';
-  });
-}
+const container_card = document.getElementById('container_card')
+
+container_card.addEventListener('mouseover', () => {
+  lista.style.display = 'flex';
+  card.style.display = 'none';
+});
+
+container_card.addEventListener('mouseout', () => {
+  lista.style.display = 'none';
+  card.style.display = 'flex';
+});
+
