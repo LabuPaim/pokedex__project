0e5ee1e14c3c1e83c976ec02bcec7b67789bb857
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-23 00:36:44 -0300

    v 3.5

--- index.js
+++ index.js
@@ -14,42 +14,47 @@ const pokedex = [
     tipo: 'Planta',
     categoria: 'Semente',
     habilidade: 'Superar',
-    altura: 50,
+    altura: '50',
     peso: '15',
     imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
     detalhes: 'Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.',
   },
   {
-    id: '004',
-    nome: 'Charmander',
-    
-    
-    peso: '8,5',
-    imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+    id: '007',
+    nome: 'Squirtle',
+    tipo: 'Água',
+    categoria: 'Tartaruga',
+    habilidade: 'Torrente',
+    altura: "50",
+    peso: '9',
+    imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+    detalhes: 'Quando retrai seu longo pescoço em sua concha, esguicha água com força vigorosa.',
     
   },
 ];
-const detalhes = [
-  {
-    detalhes:'',
-    categoria:'',
-    habilidade:'',
-    peso:'',
-    altura:'',
-  }
-]
+
 app.get('/', (req, res) => {
   res.render('index', { pokedex });
 });
 
-app.get('/detalhes', (req, res) => {
-  res.render('index', { detalhes });
+app.get('/detalhes', (req, res) => {  
+  res.render('detalhes', { pokedex });
+});
+
+app.get('/cadastro', (req, res) => {  
+  res.render('cadastro', { pokedex });
 });
 
 app.post('/add', (req, res) => {
   const pokemon = req.body;
   pokedex.push(pokemon);
   res.redirect('/');
+});
+
+app.post('/cadastro', (req, res) => {
+  const pokemon = req.body;
+  pokedex.push(pokemon);
+  res.redirect('/cadastro');
 });
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,18 @@
+
+/*================================================================= ADD Click no ADD Main =================================================================*/
+
 const addCLICK = document.getElementById('card_add');
 const formCLICK = document.getElementById('card_form');
-
-const card = document.getElementById('card');
-const lista = document.getElementById('lista');
 
 addCLICK.addEventListener('click', () => {
   formCLICK.style.display = 'flex';
   addCLICK.style.display = 'none';
 });
 
+/*==================================================================== ADD Hover Main ====================================================================*/
+
+const card = document.getElementById('card');
+const lista = document.getElementById('lista');
 
 const container_card = document.getElementById('container_card')
 
@@ -22,3 +26,22 @@ container_card.addEventListener('mouseout', () => {
   card.style.display = 'flex';
 });
 
+/*==================================================================== ADD Hover POKEBOLA NAV ====================================================================*/
+
+const pokeLINE1 = document.getElementById('poke-line1')
+const pokeLINE2 = document.getElementById('poke-line2')
+const poke = document.getElementById('container_navbar_itens_pokedex')
+
+poke.addEventListener('mouseover', () => {    
+    pokeLINE1.style.backgroundColor ='#fff';
+    pokeLINE2.style.backgroundColor ='#fff';
+    pokeLINE1.styleCadastro.backgroundColor ='#fff';
+    pokeLINE2.styleCadastro.backgroundColor ='#fff';
+})
+
+poke.addEventListener('mouseleave', () => {    
+    pokeLINE1.style.backgroundColor ='rgb(85, 84, 84)';
+    pokeLINE2.style.backgroundColor ='rgb(85, 84, 84)';
+    pokeLINE1.styleCadastro.backgroundColor ='rgb(85, 84, 84)';
+    pokeLINE2.styleCadastro.backgroundColor ='rgb(85, 84, 84)';
+})
