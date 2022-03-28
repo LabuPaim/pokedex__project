2bdd90ce64f76eca356eb02b4ca9a82fa7ef4983
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-27 21:34:54 -0300

    responsividade main

--- public/js/script.js
+++ public/js/script.js
@@ -8,23 +8,6 @@ addCLICK.addEventListener('click', () => {
   addCLICK.style.display = 'none';
 });
 
-/*==================================================================== ADD Hover Main ====================================================================*/
-
-const card = document.getElementById('card');
-const lista = document.getElementById('lista');
-
-const container_card = document.getElementById('container_card');
-
-container_card.addEventListener('mouseover', () => {
-  lista.style.display = 'flex';
-  card.style.display = 'none';
-});
-
-container_card.addEventListener('mouseout', () => {
-  lista.style.display = 'none';
-  card.style.display = 'flex';
-});
-
 /*==================================================================== ADD Hover POKEBOLA NAV ====================================================================*/
 
 const pokeLINE1 = document.getElementById('poke-line1');
